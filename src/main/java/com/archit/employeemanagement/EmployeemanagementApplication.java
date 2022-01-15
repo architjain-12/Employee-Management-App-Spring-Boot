6cbb5d01d486bc862ebf10206467729b25134386
architjain-12 <architjain08@gmail.com>
2022-01-15 13:20:13 +0530

    Starter boilerplate code

---
+++ src/main/java/com/archit/employeemanagement/EmployeemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.archit.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archit/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archit.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
